aee41b97f429ded8878b42ab2a6f6a00eae4aa1d
PZ808 <106532737+PZ808@users.noreply.github.com>
2022-06-16 14:36:16 +0200

    group by lab commit

---
+++ ihL16_06_22/group_by_queries.sql
@@ -0,0 +1,50 @@
+use sakila;
+/*
+In the table actor, what last names are not repeated? 
+For example if you would sort the data in the table actor by last_name, 
+you would see that there is Christian Arkoyd, Kirsten Arkoyd, and Debbie Arkoyd. 
+These three actors have the same last name. So we do not want to include this last name in our output. 
+Last name "Astaire" is present only one time with actor "Angelina Astaire", 
+hence we would want this in our output list. */
+
+select   distinct last_name -- , actor_id
+ from actor
+ group by actor_id
+order by last_name;
+
+select   actor_id, last_name -- , actor_id
+ from actor
+ group by actor_id
+order by last_name;
+/* Which last names appear more than once? We would use the
+ same logic as in the previous question but this time we want to include the last
+ names of the actors where the last name was present more than once */
+SELECT last_name, last_name, COUNT(last_name) as counts
+FROM actor
+GROUP BY last_name
+HAVING  counts > 1;
+
+SELECT s.first_name, s.last_name, COUNT(r.rental_id) as num_rentals, staff_id
+FROM staff  as s
+JOIN rental as r USING(staff_id)
+GROUP BY staff_id
+ORDER BY num_rentals desc;
+
+/* Using the film table, find out how many films there are of each rating. */
+SELECT COUNT( distinct title) as num_titles , rating
+FROM film
+GROUP BY rating
+ORDER BY rating ;
+/* What is the mean length of the film for each rating type. 
+Round off the average lengths to two decimal places */
+SELECT  ROUND(AVG(length),2) as avg_length , rating
+FROM film
+GROUP BY rating
+ORDER BY avg_length;
+
+/* Which kind of movies (rating) have a mean duration of more than two hours? */
+SELECT  ROUND(AVG(length),2) as avg_length , rating
+FROM film
+GROUP BY rating HAVING avg_length > 120
+ORDER BY avg_length;
+

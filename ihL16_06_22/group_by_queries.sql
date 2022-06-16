873bb1f6ff7d1272ca5c60f6333c3f2d2fabc3fa
PZ808 <106532737+PZ808@users.noreply.github.com>
2022-06-16 16:46:56 +0200

    fixed problem 1

--- ihL16_06_22/group_by_queries.sql
+++ ihL16_06_22/group_by_queries.sql
@@ -7,44 +7,64 @@ These three actors have the same last name. So we do not want to include this la
 Last name "Astaire" is present only one time with actor "Angelina Astaire", 
 hence we would want this in our output list. */
 
-select   distinct last_name -- , actor_id
- from actor
- group by actor_id
-order by last_name;
+SELECT 
+    last_name, COUNT(last_name)
+FROM
+    actor
+GROUP BY last_name
+HAVING COUNT(last_name) < 2
+ORDER BY last_name;
 
-select   actor_id, last_name -- , actor_id
- from actor
- group by actor_id
-order by last_name;
 /* Which last names appear more than once? We would use the
  same logic as in the previous question but this time we want to include the last
  names of the actors where the last name was present more than once */
-SELECT last_name, last_name, COUNT(last_name) as counts
-FROM actor
-GROUP BY last_name
-HAVING  counts > 1;
 
-SELECT s.first_name, s.last_name, COUNT(r.rental_id) as num_rentals, staff_id
-FROM staff  as s
-JOIN rental as r USING(staff_id)
+SELECT 
+    SUM(a.counts)
+FROM
+    (SELECT 
+        last_name, COUNT(last_name) AS counts
+    FROM
+        actor
+    GROUP BY last_name
+    HAVING counts > 1) AS a
+GROUP BY a.last_name
+;
+/* Using the rental table, find out how many rentals were processed by each employee. */
+SELECT 
+    s.first_name,
+    s.last_name,
+    COUNT(r.rental_id) AS num_rentals,
+    staff_id
+FROM
+    staff AS s
+        JOIN
+    rental AS r USING (staff_id)
 GROUP BY staff_id
-ORDER BY num_rentals desc;
+ORDER BY num_rentals DESC;
 
 /* Using the film table, find out how many films there are of each rating. */
-SELECT COUNT( distinct title) as num_titles , rating
-FROM film
+SELECT 
+    COUNT(DISTINCT title) AS num_titles, rating
+FROM
+    film
 GROUP BY rating
-ORDER BY rating ;
+ORDER BY rating;
 /* What is the mean length of the film for each rating type. 
 Round off the average lengths to two decimal places */
-SELECT  ROUND(AVG(length),2) as avg_length , rating
-FROM film
+SELECT 
+    ROUND(AVG(length), 2) AS avg_length, rating
+FROM
+    film
 GROUP BY rating
 ORDER BY avg_length;
 
 /* Which kind of movies (rating) have a mean duration of more than two hours? */
-SELECT  ROUND(AVG(length),2) as avg_length , rating
-FROM film
-GROUP BY rating HAVING avg_length > 120
+SELECT 
+    ROUND(AVG(length), 2) AS avg_length, rating
+FROM
+    film
+GROUP BY rating
+HAVING avg_length > 120
 ORDER BY avg_length;
 
